496fdf02303c52a936a771b2cbfd05ee9a55e674
Manuel Paniagua <manuel.paniagua84@gmail.com>
2018-10-26 12:56:26 -0700

    add about page

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
     })
 
     var scroll_start = 0;
-    var startchange = $('#about');
+    var startchange = $('#jumbotron');
     var links = $('.navbar-scroll .navbar-nav .nav-link')
     var offset = startchange.offset();
 
     if (startchange.length) {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
-            if (scroll_start > offset.top - 90) {
+            if (scroll_start > offset.top) {
                 $(".navbar-scroll").css({ 'background-color': '#fff', 'box-shadow': '0 4px 6px 0 hsla(0, 0%, 0%, .2)' });
                 $('.navbar').removeClass('navbar-dark').addClass('navbar-light');
                 // links.removeClass('light').addClass('dark');
